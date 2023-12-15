363375778b68e198d641eca1144f42c22048d7d3
fw20-0769 <tarakkarmakar1434@gmail.com>
2023-12-15 16:19:28 +0530

    userModel edited

--- Model/userModel.js
+++ Model/userModel.js
@@ -1,11 +1,6 @@
 import  mongoose from "mongoose"
 
 const userSchema = mongoose.Schema({
-  userID: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   name: {
     type: String,
     required: true,
